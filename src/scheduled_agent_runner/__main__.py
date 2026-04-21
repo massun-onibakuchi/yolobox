63703e18d0e24e2d7f1bf5abac1d6b884bffe47a
Bakuchi <49754494+massun-onibakuchi@users.noreply.github.com>
2026-04-21 23:27:11 +0800

    Revert "Add scheduled agent runner (#2)" (#3)
    This reverts commit f0bed8398a7ebdbcd9dad4505c0a1666ad9cf0bc.

--- src/scheduled_agent_runner/__main__.py
+++
@@ -1,8 +0,0 @@
-"""Where: module entrypoint. What: delegate to the CLI main function. Why: support python -m scheduled_agent_runner."""
-
-from .cli import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
-
